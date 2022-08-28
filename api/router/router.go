6c870fdbbc568b1eea387b8e572beca2fa3cbee0
oryzasa <rivaldosilalahi13@gmail.com>
2022-08-28 14:31:32 +0700

    first commit

---
+++ api/router/router.go
@@ -0,0 +1,29 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
+
+	controller "sre-test/api/controllers"
+	middleware "sre-test/api/middlewares"
+)
+
+func RegisterRoutes(db *gorm.DB) *mux.Router {
+	router := mux.NewRouter()
+
+	FoodController := controller.FoodController{}
+
+	router.HandleFunc("/food/all", middleware.CheckAuth(FoodController.GetAllFoodItems(db))).Methods(http.MethodGet)
+	router.HandleFunc("/food", middleware.CheckAuth(FoodController.AddNewFoodItem(db))).Methods(http.MethodPost)
+	router.HandleFunc("/food/{name}", middleware.CheckAuth(FoodController.GetSingleFoodItem(db))).Methods(http.MethodGet)
+	router.HandleFunc("/food/{id}", middleware.CheckAuth(FoodController.DeleteSingleFoodItem(db))).Methods(http.MethodDelete)
+
+	UserController := controller.UserController{}
+
+	router.HandleFunc("/auth/login", UserController.LoginUser(db)).Methods(http.MethodPost)
+	router.HandleFunc("/auth/signup", UserController.SignupUser(db)).Methods(http.MethodPost)
+
+	return router
+}
